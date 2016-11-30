d28f44fb57e388d8e4f262e88c0403b1f7dedb64
Chris <orthagonal1@gmail.com>
2016-11-30 13:23:02 -0600

    export unified process flow + individual funcs

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const imageminPngquant = require('imagemin-pngquant');
 const Svgo = require('svgo');
 const fs = require('fs');
 const os = require('os');
+const async = require('async');
+const path = require('path');
 
-module.exports.compress = (file, level, callback) => {
+const compress = (file, level, callback) => {
   const handleResult = result => {
     if (result.length === 0) {
       return callback('Unable to compress image');
@@ -19,7 +21,7 @@ module.exports.compress = (file, level, callback) => {
   imagemin([file], os.tmpdir(), options).then(handleResult);
 };
 
-module.exports.svgOptimize = (file, callback) => {
+const svgOptimize = (file, callback) => {
   fs.readFile(file, 'utf8', (err, data) => {
     if (err) {
       return callback(err);
@@ -30,7 +32,7 @@ module.exports.svgOptimize = (file, callback) => {
   });
 };
 
-module.exports.crop = (im, pathToFile, origin, dims, gravity, callback) => {
+const crop = (im, pathToFile, origin, dims, gravity, callback) => {
   const gm = require('gm').subClass({ imageMagick: im });
   const pathToOutput = pathToFile;
   gm(pathToFile)
@@ -40,3 +42,39 @@ module.exports.crop = (im, pathToFile, origin, dims, gravity, callback) => {
     callback(err, pathToOutput);
   });
 };
+
+module.exports.process = (imageFilePath, options, callback) => {
+  async.auto({
+    optimizeSvg: (done) => {
+      if (path.extname(imageFilePath).toLowerCase() === '.svg') {
+        return svgOptimize(imageFilePath, done);
+      }
+      done(null, imageFilePath);
+    },
+    compress: ['optimizeSvg', (results, done) => {
+      if (options.quality && options.quality !== 100) {
+        return compress(imageFilePath, options.quality, (err, result) => {
+          done(err, result);
+        });
+      }
+      done(null, imageFilePath);
+    }],
+    crop: ['compress', (results, done) => {
+      if (options.size) {
+        return crop(options.imagemagick, results.compress, options.position, options.size, options.gravity, (err, result) => {
+          done(err, result);
+        });
+      }
+      done(null, results.compress);
+    }],
+  }, (err, results) => {
+    if (err) {
+      return callback(err);
+    }
+    callback(null, results.crop);
+  });
+};
+
+module.exports.compress = compress;
+module.exports.svgOptimize = svgOptimize;
+module.exports.crop = crop;
