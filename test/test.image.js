9ccd9c7aad83d380199df50207685bc6dbb15647
orthagonal <orthagonal1@gmail.com>
2019-05-09 17:31:53 +0000

    new version

--- test/test.image.js
+++ test/test.image.js
@@ -1,5 +1,4 @@
-/* eslint-disable no-undef */
-const chai = require('chai');
+const tap = require('tap');
 const optimiz = require('../index.js');
 const fs = require('fs');
 const path = require('path');
@@ -8,95 +7,69 @@ const testImage = path.join(__dirname, testImageBase);
 const testSVGBase = 'snoopy-pilot.svg';
 const testSVG = path.join(__dirname, testSVGBase);
 const os = require('os');
-const useImageMagick = true; // can change this to false to use GraphicsMagick
 
 let testImageSize = 0;
 let testSVGSize = 0;
 
-describe('can be used as a library', () => {
-  before((done) => {
-    testImageSize = fs.statSync(testImage).size;
-    testSVGSize = fs.statSync(testSVG).size;
-    done();
-  });
-  // copy an image to temp
-  const makeTempFile = (originalFilePath, done) => {
+testImageSize = fs.statSync(testImage).size;
+testSVGSize = fs.statSync(testSVG).size;
+
+// copy an image to temp
+const makeTempFile = (originalFilePath) => {
+  return new Promise(success => {
     const tmpFilePath = path.join(os.tmpdir(), path.basename(originalFilePath));
     const writeStream = fs.createWriteStream(tmpFilePath);
     writeStream.on('finish', () => {
-      done(tmpFilePath);
+      success(tmpFilePath);
     });
     fs.createReadStream(originalFilePath).pipe(writeStream);
-  };
+  });
+};
 
-  it('should be able to crop an image', (done) => {
-    makeTempFile(testImage, (fileName) => {
-      const options = {
-        imagemagick: useImageMagick,
-        position: [20, 20],
-        size: [100, 100]
-      };
-      optimiz(fileName, options, (err, outputFile) => {
-        chai.expect(err).to.equal(null);
-        chai.expect(fs.statSync(outputFile).size).to.be.below(testImageSize);
-        done();
-      });
-    });
-  });
-  it('should be able to compress an image', (done) => {
-    makeTempFile(testImage, (fileName) => {
-      const options = {
-        imagemagick: useImageMagick,
-        quality: 50
-      };
-      optimiz(fileName, options, (err, outputFile) => {
-        chai.expect(err).to.equal(null);
-        chai.expect(fs.statSync(outputFile).size).to.be.below(testImageSize);
-        done();
-      });
-    });
-  });
-  it('should be able to optimize an svg image with svgo', (done) => {
-    makeTempFile(testSVG, (fileName) => {
-      const options = {
-        imagemagick: useImageMagick,
-        quality: 50
-      };
-      optimiz(fileName, options, (err, outputFile) => {
-        chai.expect(err).to.equal(null);
-        chai.expect(fs.statSync(outputFile).size).to.be.below(testSVGSize);
-        done();
-      });
-    });
-  });
-  it('should be able to crop/compress an image without error', (done) => {
-    makeTempFile(testImage, (fileName) => {
-      const options = {
-        imagemagick: useImageMagick,
-        quality: 80,
-        position: [10, 10],
-        size: [120, 120]
-      };
-      optimiz(fileName, options, (err, outputFile) => {
-        chai.expect(err).to.equal(null);
-        chai.expect(fs.statSync(outputFile).size).to.be.below(testImageSize);
-        done();
-      });
-    });
-  });
-  it('should be able to crop/compress an svg image without error', (done) => {
-    makeTempFile(testSVG, (fileName) => {
-      const options = {
-        imagemagick: useImageMagick,
-        quality: 80,
-        position: [10, 10],
-        size: [120, 120]
-      };
-      optimiz(fileName, options, (err, outputFile) => {
-        chai.expect(err).to.equal(null);
-        chai.expect(fs.statSync(outputFile).size).to.be.below(testSVGSize);
-        done();
-      });
-    });
-  });
+tap.test('should be able to resize an image', async t => {
+  const fileBuffer = await makeTempFile(testImage);
+  const options = {
+    resizeMethodName: 'resize',
+    width: 50,
+    height: 50,
+    background: 'green'
+  };
+  const outputFile = await optimiz.resize(options, fs.readFileSync(fileBuffer));
+  // fs.writeFileSync('theoutput.jpg', outputFile);
+  t.ok(outputFile.length < testImageSize);
+  t.end();
+});
+
+tap.test('it should be able to compress an image', async t => {
+  const fileBuffer = await makeTempFile(testImage);
+  const options = {
+    quality: 50
+  };
+  const outputFile = await optimiz.optimize(options, fs.readFileSync(fileBuffer));
+  fs.writeFileSync('theoutput2.jpg', outputFile);
+  t.ok(outputFile.length < testImageSize);
+  t.end();
+});
+
+tap.test('it should be able to compress an svg image', async t => {
+  const fileBuffer = await makeTempFile(testSVG);
+  const options = {
+    quality: 50
+  };
+  const outputFile = await optimiz.optimize(options, fs.readFileSync(fileBuffer));
+  // fs.writeFileSync('theoutput2.svg', outputFile);
+  t.ok(outputFile.length < testSVGSize);
+  t.end();
+});
+
+tap.test('it should be able to thumbnail an image', async t => {
+  const fileBuffer = await makeTempFile(testImage);
+  const options = {
+    width: 10,
+    height: 10
+  };
+  const outputFile = await optimiz.thumbnail(options, fs.readFileSync(fileBuffer));
+  // fs.writeFileSync('theoutput3.jpg', outputFile);
+  t.ok(outputFile.length < testImageSize);
+  t.end();
 });
