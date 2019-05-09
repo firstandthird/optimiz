b87727f55bf48362278bf86c07307c8732418021
orthagonal <orthagonal1@gmail.com>
2019-05-09 17:36:40 +0000

    remove comments

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 
-// get image file info either from a url or from a file path:
-// resize the image:
 module.exports.resize = require('./lib/resize');
 module.exports.optimize = require('./lib/optimize');
 module.exports.thumbnail = require('./lib/thumbnail');
-//
-// module.exports.handler = async(event) => {
-//   await processIncomingEvent(new Interpreter(env), event);
-// };
